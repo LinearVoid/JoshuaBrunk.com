aa91ccc808091f0b289746b05f140f0370261c57
Joshua Brunk <26190850+LinearVoid@users.noreply.github.com>
2023-03-15 21:13:51 -0400

    Update Projects.js

--- personal-site/src/Components/Projects.js
+++ personal-site/src/Components/Projects.js
@@ -15,10 +15,10 @@ function Projects(){
             <div className = "flexarrange">
            
                 <Card  alt = "Image of Waving Robot, my Logo"name= "JoshuaBrunk.com" photo = {bot} phrase = "Check out the Github Repo for this site by clicking here!" url= "https://github.com/LinearVoid/JoshuaBrunk.com " />
-                <Card  alt = "Meme Generator"name= "Meme generator" photo = "https://orig00.deviantart.net/c96c/f/2011/288/8/c/troll_face_sad_by_rober_raik-d4cwjk7.png" phrase = "A simple meme generator I created" url= "https://linearvoid.github.io/Meme-generator/ " />
                 <Card alt = "Image of a clock" name= "Neomorphism Timer" photo = {clock} phrase = "A Simple Timer made in REACT" url = "https://linearvoid.github.io/CoolReactTimer/" />
                 <Card alt = "binary search image" name= "Simple Pyinary Search" photo = "https://s3-us-west-2.amazonaws.com/ib-assessment-tests/problem_images/binary_search.gif" phrase = "A simple Python Script I created while trying to understand the CORDIC algorithm" url= "https://github.com/LinearVoid/SimplePyinarySearch" />
                 <Card alt = "image of perlin noise" name= "Perlin Noise Generator" photo = {perlin} phrase = "A simple program that generates 2D perlin noise using Unity" url= "https://linearvoid.itch.io/perlin-noise-viewer-test" />
+                <Card  alt = "Meme Generator"name= "Meme generator" photo = "https://orig00.deviantart.net/c96c/f/2011/288/8/c/troll_face_sad_by_rober_raik-d4cwjk7.png" phrase = "A simple meme generator I created" url= "https://linearvoid.github.io/Meme-generator/ " />
                 
                 </div>
             
